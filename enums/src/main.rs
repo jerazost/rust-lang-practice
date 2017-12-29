042a81e54c355fbe423124278fa0827e90037b2e
Juan Erazo <fersot100@yahoo.com>
2017-12-29 15:16:15 -0800

    adding individual gitignore to enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -4,9 +4,26 @@ enum IpAddr {
     V6(String),
 }
 
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+
+    }
+}
+
 fn main() {
     let home = IpAddr::V4(127, 0, 0, 1);
     let loopback = IpAddr::V6(String::from("::1"));
+    
+    let m = Message::Write(String::from("Hello"));
+    m.call();
+        
     println!("{:?}", home);
     println!("{:?}", loopback);
 }
